d4d1c6cf4db98599c67f54db715cc799c5c225a6
Rahul <v.rahul1719@gmail.com>
2019-09-14 15:58:23 +0530

    Java Programs

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) throws IOException {
+        /*BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String input =in.readLine();
+        System.out.println(input);
+        */
+        fizzBuzz();
+    }
+
+    private static void fizzBuzz() {
+        for (int i=0;i<=100 ;i ++){
+            Object number = i%3 == 0 ? "Fizz" : i%5==0 ? "Buzz" : i;
+            System.out.println(number.toString());
+        }
+    }
+}
